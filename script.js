639e3bc219afb2939f8aa579f86fe42f40496fa2
micky06 <michael-michaux_student2021@wilder.school>
2021-12-17 11:38:47 +0100

    essai de menu page printers

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-fetch("https://cults3d.com/fr/blog.rss")
-.then(async resp => {
-    let body = await resp.text();
-    body = new DOMParser().parseFromString(body, 'text/xml')
-    console.log(body)
-    let items = body.querySelectorAll("item")
-    let str = "";
-    for (const item of items) {
-        const title = item.querySelector("title")
-        const image = item.querySelector("image");
-        const description = item.querySelector("description");
-        str += `
-<article>
-<h3>${title.innerHTML}</h3>
-<figure>
-<img src="${image.innerHTML}" alt="">
-<figcaption>${description.innerHTML}</figcaption>
-</figure>
-</article>`
-    }
-        document.querySelector("#container > section").innerHTML = str;
-    console.log(items);
-})
+const menuPrinters = document.querySelectorAll('.iconMenuPrinters');
+const pageFDM = document.querySelector('.fdm')
+const pageFDMEnfant = document.querySelector('.fdmEnfant')
+const pageResine = document.querySelector('.resine')
+const pageResineEnfant = document.querySelector('.resineEnfant')
+const pageTDP = document.querySelector('.troisDP')
+const pageTDPEnfant = document.querySelector('.troisDPEnfant')
+const transition = document.querySelector('.cercle')
+for (let i = 0; i < menuPrinters.length; i++) {
+    pageFDM.classList.add("apparait");
+    pageFDMEnfant.classList.add("apparaitEnfant");
+    menuPrinters[i].addEventListener('click', function () {
+        if (menuPrinters[i].alt === "FDM") {
+            // transition.classList.toggle("zoom")
+            pageFDM.classList.add("apparait");
+            pageFDMEnfant.classList.add("apparaitEnfant");
+            pageResine.classList.remove("apparait");
+            pageResineEnfant.classList.remove("apparaitEnfant");
+            pageTDP.classList.remove("apparait");
+            pageTDPEnfant.classList.remove("apparaitEnfantEnfant");
+        } else if (menuPrinters[i].alt === "SLA") {
+            // transition.classList.toggle("zoom")
+            pageFDM.classList.remove("apparait");
+            pageFDMEnfant.classList.remove("apparaitEnfant");
+            pageResine.classList.add("apparait");
+            pageResineEnfant.classList.add("apparaitEnfant");
+            pageTDP.classList.remove("apparait");
+            pageTDPEnfant.classList.remove("apparaitEnfant");
+        } else if (menuPrinters[i].alt === "3DP") {
+            // transition.classList.toggle("zoom")
+            pageFDM.classList.remove("apparait");
+            pageFDMEnfant.classList.remove("apparaitEnfant");
+            pageResine.classList.remove("apparait");
+            pageResineEnfant.classList.remove("apparaitEnfant");
+            pageTDP.classList.add("apparait");
+            pageTDPEnfant.classList.add("apparaitEnfant");
+        }
+    })
+
+}
